1807972b265a4f86982a2147477e285093eecf0c
BlackDragon <73701060+BlackDragon0207@users.noreply.github.com>
2021-12-24 18:25:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { token, default_prefix } = require('./config.json');
-const { readdirSync } = require('fs');
-const { join } = require('path');
-const config = require('./config.json');
-client.config = config;
-const db = require('quick.db');
 const message = [' ']
 let current = 1;
+
+client.login('TOKEN')
 
 client.on('ready', () => {
     console.log('I am ready');
@@ -56,36 +52,3 @@ client.on('guildMemberRemove', member => {
   .setColor("#ff0606")
   client.channels.cache.get("842021478420709387").send(embed)
 })
-
-
-client.on("message", async message => {
-
-    if(message.author.bot) return;
-    if(message.channel.type === 'dm') return;
-
-    let prefix = await db.get(`prefix_${message.guild.id}`);
-    if(prefix === null) prefix = default_prefix;
-
-    const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-
-const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
-
-
-    if(prefixRegex.test(message.content)){
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-
-        const command = args.shift().toLowerCase();
-
-        if(!client.commands.has(command)) return;
-
-
-        try {
-            client.commands.get(command).run(client, message, args);
-
-        } catch (error){
-            console.error(error);
-        }
-    }
-})
-
-client.login(token)
